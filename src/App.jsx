f16499e4f9a763bc092f7a0b7195ff64e4ee4f45
Román Aquino <romanaquino88@gmail.com>
2025-06-24 17:51:16 -0300

    Comienza proyecto coder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <section className="container">
+      <h1 style={{ color: "yellow", fontSize: "200px" }}>Hello React</h1>
+    </section>
+  );
+}
 
 export default App;
